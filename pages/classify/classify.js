9cce687b0f08929412d7791fd8a887988d5ce747
wj <308655673@qq.com>
2022-04-28 16:06:34 +0800

    222222

--- pages/classify/classify.js
+++ pages/classify/classify.js
@@ -5,7 +5,37 @@ Page({
    * 页面的初始数据
    */
   data: {
-test:"测试"
+    test: "测试",
+    bool: false,
+    arr:"abcdefghi",
+    xiala:true,
+    on:true,
+    num:1
+  },
+  off(){
+    this.setData({
+      on:!this.data.on
+    })
+  },
+  picturn(event) {
+    console.log(event.detail)
+    this.setData({
+      // bool:event.detail.source=="touch"
+      // bool: event.detail.x > 150
+    })
+
+  },
+  dibu(){
+    console.log("到底了")
+  },
+  add(){
+
+this.setData({
+  arr:"0123456789abcdefghi",
+  xiala:false
+  
+})
+
   },
 
   /**
@@ -27,19 +57,26 @@ test:"测试"
    */
   onShow: function () {
 
+    setInterval(()=>{
+      this.setData({
+        num:this.data.num>=100?1:this.data.num+1
+      })
+    },1000)
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-
+console.log("分类隐藏")
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
+    console.log("分类卸载")
 
   },
 
